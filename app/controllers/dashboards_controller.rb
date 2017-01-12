232d41c90897ef8e0331234ac8fa6ab33e2fb695
Lorna Marlow <lornamarlow@hotmail.co.uk>
2017-01-12 17:21:43 +0000

    Add patients and appointments

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,6 @@
 class DashboardsController < ApplicationController
   def main
-
+    @new_patients = Patient.where(created_at: Time.current.all_week).count
+    #@income = Income.where(created_at: Time.current.all_week)
   end
 end
